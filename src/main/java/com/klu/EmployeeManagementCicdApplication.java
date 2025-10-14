5525b4726b8129d13189246d22858ab0d78cc203
saradeepika03@gmail.com <saradeepika03@gmail.com>
2025-10-14 22:48:05 +0530

    EmployeeManagementCicdApplication.java

--- src/main/java/com/klu/EmployeeManagementCicdApplication.java
+++ src/main/java/com/klu/EmployeeManagementCicdApplication.java
@@ -1,16 +1,16 @@
-package com.klu;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class EmployeeManagementCicdApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(EmployeeManagementCicdApplication.class, args);
-	}
-
-}
+//package com.klu;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class EmployeeManagementCicdApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(EmployeeManagementCicdApplication.class, args);
+//	}
+//
+//}
 
 //package com.klu;
 //
@@ -31,3 +31,26 @@ public class EmployeeManagementCicdApplication {
 //        SpringApplication.run(EmployeeManagementCicdApplication.class, args);
 //    }
 //}
+
+
+package com.klu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer; // <-- ADD THIS IMPORT
+
+@SpringBootApplication
+// 1. EXTEND SpringBootServletInitializer
+public class EmployeeManagementCicdApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementCicdApplication.class, args);
+	}
+
+    // 2. ADD THE CONFIGURE METHOD
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EmployeeManagementCicdApplication.class);
+    }
+}
